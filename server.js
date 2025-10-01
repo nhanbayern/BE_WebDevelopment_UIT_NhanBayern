4f43431642490fb2b206496ab9d07aa7f5049323
Nhan Bayern <nhanbayern@gmail.com>
2025-10-01 22:07:17 +0700

    Sửa file server.js 22:07 01-10-2025

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require("express");
+const express = require("express"); // commonjs
+// import experess from 'express"
 const mysql = require("mysql2");
 const cors = require("cors");
 require("dotenv").config();
-
-const app = express();
+var router1 = express.Router();
+const app = express(); // app express
+// Chạy server
+const PORT = process.env.PORT || 5000; // khai báo port
 app.use(express.json());
 app.use(cors());
 
@@ -13,6 +16,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  charset: "utf8mb4",
 });
 db.connect((err) => {
   if (err) {
@@ -21,13 +25,14 @@ db.connect((err) => {
   }
   console.log("✅ DB connected");
 });
+//KHAI BÁO ROUTE
 app.get("/", (req, res) => {
   res.send("Đây là web application, được chạy với mysql.Xin chào mọi người !!");
 });
 
 // CRUD viết trực tiếp ở đây
 app.get("/products", (req, res) => {
-  db.query("SELECT * FROM products", (err, results) => {
+  db.query("SELECT * FROM  usda_food_nutrition LIMIT 20;", (err, results) => {
     if (err) return res.status(500).json({ error: err.message });
     res.json(results);
   });
@@ -41,9 +46,17 @@ app.post("/products", (req, res) => {
     res.json({ id: result.insertId, name, price, category });
   });
 });
+router1.get("/", (req, res) => {
+  res.json("this is router 1");
+  console.log("running router 1");
+});
+app.use("/duongdan1", router1);
+//this is localhost:3000/duongdan1//cuar router
 
-// Chạy server
-const PORT = process.env.PORT || 5000;
+router1.get("/giohang", (req, res) => {
+  res.send("Xin moi ban vao gio hang");
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
