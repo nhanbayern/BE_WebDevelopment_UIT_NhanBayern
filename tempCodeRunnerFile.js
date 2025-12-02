678531e5d84f841034349dfc1328737c903f07b6
nhanbayern <147220729+nhanbayern@users.noreply.github.com>
2025-12-02 11:03:10 +0700

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-// hash.mjs
-import bcrypt from "bcryptjs";
-
-const [, , password = "HelloWorld", rounds = "10"] = process.argv;
-//nguyễn thiện nhân: 123
-//đỗ hoàng phúc: 1234
-const hash = await bcrypt.hash(password, parseInt(rounds, 10));
-console.log(hash);
-// tạo tài khoản hash vào trong dbms
